d5f6c191c9e2847006b012e0c854ced034bc7689
박이완 <dy6153@naver.com>
2024-03-07 10:44:45 +0900

    Delete devel, build

--- catkin_ws/build/MORAI-DriveExample_ROS/morai_standard/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/catkin_ws/src/MORAI-DriveExample_ROS/morai_standard
-# Build directory: /home/ubuntu/catkin_ws/build/MORAI-DriveExample_ROS/morai_standard
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
