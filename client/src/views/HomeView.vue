4838d3cd3c06270dfeb8856ef16a3d39260e6cb8
양성호 <tipan33@naver.com>
2024-03-25 17:01:15 +0900

    client skeleton build

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { GoogleMap, Marker } from 'vue3-google-map'
+import { env } from '../stores/env'
+
+const apiKey = env.apiKey
+const center = { lat: 37.242819, lng: 126.775207 }
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main> 
+    <div>
+      <GoogleMap
+      :api-key= apiKey
+      style="width: 75%; height: 500px"
+      :center="center"
+      :zoom="17"
+      >
+      <Marker :options="{ position: center }" />
+    </GoogleMap>
+  </div>
+  <div></div>
+</main>
 </template>
+
+<style scoped>
+
+</style>
