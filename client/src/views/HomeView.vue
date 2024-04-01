3704b49edf1b5289ba105a586b40cb69dca0d8c4
양성호 <tipan33@naver.com>
2024-04-01 14:35:59 +0900

    modified code

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -3,25 +3,26 @@ import { GoogleMap, Marker } from 'vue3-google-map'
 import { env } from '../stores/env'
 
 const apiKey = env.apiKey
-const center = { lat: 37.238077, lng: 126.773033 }
+const patients = { lat: 37.238077, lng: 126.773033 } // 환자 위치
+const destination = { lat: 37.238931, lng: 126.772997 } // 목적지 위치
 </script>
 
 <template>
   <main> 
     <div>
       <GoogleMap
-      :api-key= apiKey
-      style="width: 75%; height: 500px"
-      :center="center"
-      :zoom="17"
+        :api-key="apiKey"
+        style="width: 75%; height: 500px"
+        :center="patients"
+        :zoom="17"
       >
-      <Marker :options="{ position: center }" />
-    </GoogleMap>
-  </div>
-  <div></div>
-</main>
+        <Marker :options="{ position: patients }" />
+        <Marker :options="{ position: destination}" />
+      </GoogleMap>
+    </div>
+    <div></div>
+  </main>
 </template>
 
 <style scoped>
-
 </style>
